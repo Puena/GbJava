4b50e192b5166862ca02867c460a2e3dee54280a
Platonov Dmitry <woweuropeluffy@gmail.com>
2022-11-26 19:38:38 +0300

    Homework 1

---
+++ Homework1.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void task1(int n){
+        int sum = 1;
+        for (int i = 1; i < n + 1; i++){
+            sum = sum * i;
+        }
+        System.out.println(sum);
+    }
+
+    public static void task2() {
+        int start = 1;
+        int stop = 1000;
+        int num = 0;
+        System.out.println("Prime numbers from 1 to 100 are :");
+        for (start = 1; start <= stop; start++)
+        {
+            int counter=0;
+            for(num =start; num>=1; num--)
+            {
+                if(start%num==0)
+                {
+                    counter = counter + 1;
+                }
+            }
+            if (counter ==2)
+            {
+                //Appended the Prime number to the String
+                System.out.printf("%d ", start);
+            }
+        }
+    }
+
+    public static void task3(){
+        double num1;
+        double num2;
+        double ans;
+        char op;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        num1 = reader.nextDouble();
+        num2 = reader.nextDouble();
+        System.out.print("\nEnter an operator (+, -, *, /): ");
+        op = reader.next().charAt(0);
+        switch (op) {
+            case '+' -> ans = num1 + num2;
+            case '-' -> ans = num1 - num2;
+            case '*' -> ans = num1 * num2;
+            case '/' -> ans = num1 / num2;
+            default -> {
+                System.out.println("Ошибка! Не известный оператор");
+                return;
+            }
+        }
+        System.out.print("\nРезультат выражения:\n");
+        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Homework1.task2();
+    }
+}
